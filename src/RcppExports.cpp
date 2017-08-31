0d2a61e620f15a0b808edc2b8203915f9a47a52a
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2017-08-31 16:46:24 +0200

    use Rcpp's native routine registration

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,7 +7,7 @@ using namespace Rcpp;
 
 // CPL_GetMetadata
 Rcpp::CharacterVector CPL_GetMetadata(Rcpp::CharacterVector obj, Rcpp::CharacterVector domain_item, Rcpp::CharacterVector options);
-RcppExport SEXP stars_CPL_GetMetadata(SEXP objSEXP, SEXP domain_itemSEXP, SEXP optionsSEXP) {
+RcppExport SEXP _stars_CPL_GetMetadata(SEXP objSEXP, SEXP domain_itemSEXP, SEXP optionsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -20,7 +20,7 @@ END_RCPP
 }
 // CPL_get_crs
 Rcpp::List CPL_get_crs(Rcpp::CharacterVector obj, Rcpp::CharacterVector options);
-RcppExport SEXP stars_CPL_get_crs(SEXP objSEXP, SEXP optionsSEXP) {
+RcppExport SEXP _stars_CPL_get_crs(SEXP objSEXP, SEXP optionsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -32,7 +32,7 @@ END_RCPP
 }
 // CPL_gdal_init
 void CPL_gdal_init();
-RcppExport SEXP stars_CPL_gdal_init() {
+RcppExport SEXP _stars_CPL_gdal_init() {
 BEGIN_RCPP
     Rcpp::RNGScope rcpp_rngScope_gen;
     CPL_gdal_init();
@@ -41,7 +41,7 @@ END_RCPP
 }
 // CPL_gdal_cleanup_all
 void CPL_gdal_cleanup_all();
-RcppExport SEXP stars_CPL_gdal_cleanup_all() {
+RcppExport SEXP _stars_CPL_gdal_cleanup_all() {
 BEGIN_RCPP
     Rcpp::RNGScope rcpp_rngScope_gen;
     CPL_gdal_cleanup_all();
@@ -50,7 +50,7 @@ END_RCPP
 }
 // CPL_gdal_version
 const char* CPL_gdal_version(const char* what);
-RcppExport SEXP stars_CPL_gdal_version(SEXP whatSEXP) {
+RcppExport SEXP _stars_CPL_gdal_version(SEXP whatSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -61,7 +61,7 @@ END_RCPP
 }
 // CPL_proj_version
 std::string CPL_proj_version(bool b);
-RcppExport SEXP stars_CPL_proj_version(SEXP bSEXP) {
+RcppExport SEXP _stars_CPL_proj_version(SEXP bSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -72,7 +72,7 @@ END_RCPP
 }
 // CPL_proj_is_valid
 Rcpp::List CPL_proj_is_valid(std::string proj4string);
-RcppExport SEXP stars_CPL_proj_is_valid(SEXP proj4stringSEXP) {
+RcppExport SEXP _stars_CPL_proj_is_valid(SEXP proj4stringSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -83,7 +83,7 @@ END_RCPP
 }
 // CPL_gdalinfo
 Rcpp::CharacterVector CPL_gdalinfo(Rcpp::CharacterVector obj, Rcpp::CharacterVector options);
-RcppExport SEXP stars_CPL_gdalinfo(SEXP objSEXP, SEXP optionsSEXP) {
+RcppExport SEXP _stars_CPL_gdalinfo(SEXP objSEXP, SEXP optionsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -95,7 +95,7 @@ END_RCPP
 }
 // CPL_gdalwarp
 Rcpp::LogicalVector CPL_gdalwarp(Rcpp::CharacterVector src, Rcpp::CharacterVector dst, Rcpp::CharacterVector options);
-RcppExport SEXP stars_CPL_gdalwarp(SEXP srcSEXP, SEXP dstSEXP, SEXP optionsSEXP) {
+RcppExport SEXP _stars_CPL_gdalwarp(SEXP srcSEXP, SEXP dstSEXP, SEXP optionsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -108,7 +108,7 @@ END_RCPP
 }
 // CPL_gdalrasterize
 Rcpp::LogicalVector CPL_gdalrasterize(Rcpp::CharacterVector src, Rcpp::CharacterVector dst, Rcpp::CharacterVector options);
-RcppExport SEXP stars_CPL_gdalrasterize(SEXP srcSEXP, SEXP dstSEXP, SEXP optionsSEXP) {
+RcppExport SEXP _stars_CPL_gdalrasterize(SEXP srcSEXP, SEXP dstSEXP, SEXP optionsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -118,4 +118,23 @@ BEGIN_RCPP
     rcpp_result_gen = Rcpp::wrap(CPL_gdalrasterize(src, dst, options));
     return rcpp_result_gen;
 END_RCPP
+}
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_stars_CPL_GetMetadata", (DL_FUNC) &_stars_CPL_GetMetadata, 3},
+    {"_stars_CPL_get_crs", (DL_FUNC) &_stars_CPL_get_crs, 2},
+    {"_stars_CPL_gdal_init", (DL_FUNC) &_stars_CPL_gdal_init, 0},
+    {"_stars_CPL_gdal_cleanup_all", (DL_FUNC) &_stars_CPL_gdal_cleanup_all, 0},
+    {"_stars_CPL_gdal_version", (DL_FUNC) &_stars_CPL_gdal_version, 1},
+    {"_stars_CPL_proj_version", (DL_FUNC) &_stars_CPL_proj_version, 1},
+    {"_stars_CPL_proj_is_valid", (DL_FUNC) &_stars_CPL_proj_is_valid, 1},
+    {"_stars_CPL_gdalinfo", (DL_FUNC) &_stars_CPL_gdalinfo, 2},
+    {"_stars_CPL_gdalwarp", (DL_FUNC) &_stars_CPL_gdalwarp, 3},
+    {"_stars_CPL_gdalrasterize", (DL_FUNC) &_stars_CPL_gdalrasterize, 3},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_stars(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }
