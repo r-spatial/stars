5c0c911e99d965bc85f3e624b42c2d0803bc873b
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2017-10-08 20:25:40 +0200

    add tests and test output

---
+++ tests/sf.R
@@ -0,0 +1,21 @@
+library(stars)
+library(sf)
+tif = system.file("tif/L7_ETMs.tif", package = "stars")
+(x = st_stars(tif, options = c("OVERVIEW_LEVEL=3")))
+names(x) = "L7_ETM"
+image(x)
+(sfc = st_as_sfc(x, as_points = FALSE))
+plot(sfc, add  =TRUE)
+(sfc = st_as_sfc(x, as_points = TRUE))
+plot(sfc, add = TRUE)
+
+sf = st_as_sf(x, as_points = FALSE)
+plot(sf, border = NA)
+
+sfc1 <- st_as_sfc(x, as_points = TRUE, use_cpp = TRUE)
+sfc2 <- st_as_sfc(x, as_points = TRUE, use_cpp = FALSE)
+identical(sfc1, sfc2)
+
+sfc1 <- st_as_sfc(x, as_points = FALSE, use_cpp = TRUE)
+sfc2 <- st_as_sfc(x, as_points = FALSE, use_cpp = FALSE)
+identical(sfc1, sfc2)
