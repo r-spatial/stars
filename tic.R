069a72f585980701ae63de3d226ff0fee58ec776
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-04-25 17:51:49 +0200

    new travis setup

---
+++ tic.R
@@ -0,0 +1,14 @@
+add_package_checks()
+
+###
+# deploy pkgdowm site
+###
+if (Sys.getenv("id_rsa") != "") {
+
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
+
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages", orphan = TRUE))
+}
