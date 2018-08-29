f9d72fd982d11f903d229d93bdfc483c5a03cb27
pat-s <patrick.schratz@gmail.com>
2018-08-29 23:44:07 +0200

    split knitr and rmarkdown

--- tic.R
+++ tic.R
@@ -2,7 +2,8 @@ add_package_checks()
 
 get_stage("install") %>%
   add_step(step_install_cran("lwgeom", configure.args="--without-liblwgeom", lib = "/home/travis/R/Library/tic-lib")) %>%  # install tic into custom R library used for R CMD check only
-  add_step(step_install_cran(c("knitr", "rmarkdown")))
+  add_step(step_install_cran("knitr")) %>%
+  add_step(step_install_cran("rmarkdown"))
 
 ###
 # deploy pkgdowm site
