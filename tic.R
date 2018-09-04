63f012ed11dad3d28b33647c13a4ed6cf54704cb
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-09-04 18:22:50 +0200

    silence tic

--- tic.R
+++ tic.R
@@ -13,7 +13,7 @@ if (Sys.getenv("id_rsa") != "") {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
-  get_stage("deploy") %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+  #get_stage("deploy") %>%
+  #  add_step(step_build_pkgdown()) %>%
+  #  add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 }
