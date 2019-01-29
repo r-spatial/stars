96792e61878433b5d1f042a4554f2ce62b8ab4fa
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-01-29 21:41:28 +0100

    coverage

--- tests/aggregate.R
+++ tests/aggregate.R
@@ -4,8 +4,10 @@ library(spacetime)
 data(air) # this loads several datasets in .GlobalEnv
 dim(air)
 d = st_dimensions(station = st_as_sfc(stations), time = dates)
+
+blocks = st_make_grid(st_as_sfc("POLYGON ((5.871619 47.26986, 15.03811 47.26986, 15.03811 55.05653, 5.871619 55.05653, 5.871619 47.26986))", crs = 4326),
+	n = c(3,3))
+
 (aq = st_as_stars(list(PM10 = air), dimensions = d))
-(a = aggregate(aq, st_as_sf(DE_NUTS1), mean, na.rm = TRUE))
-
-
+(a = aggregate(aq, blocks, mean, na.rm = TRUE))
 
