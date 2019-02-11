f4e8ea6268706ee07634c3e514478bf498cac35f
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-02-11 16:42:57 +0100

    add connect8 option; https://github.com/r-spatial/stars/issues/99

--- R/sf.R
+++ R/sf.R
@@ -75,8 +75,9 @@ st_xy2sfc = function(x, as_points, ..., na.rm = TRUE) {
 #' @param merge logical; if \code{TRUE}, cells with identical values are merged (using \code{GDAL_Polygonize} or \code{GDAL_FPolygonize}); if \code{FALSE}, a polygon for each raster cell is returned; see details
 #' @param use_integer (relevant only if \code{merge} is \code{TRUE}): if \code{TRUE}, before polygonizing values are rounded to 32-bits signed integer values (GDALPolygonize), otherwise they are converted to 32-bit floating point values (GDALFPolygonize).
 #' @param long logical; if \code{TRUE}, return a long table form \code{sf}, with geometries and other dimensinos recycled
+#' @param connect8 logical; if \code{TRUE}, use 8 connectedness. Otherwise the 4 connectedness algorithm will be applied.
 #' @param ... ignored
-#' @details If \code{merge} is \code{TRUE}, only the first attribute is converted into an \code{sf} object. If \code{na.rm} is \code{FALSE}, areas with \code{NA} values are also written out as polygons.
+#' @details If \code{merge} is \code{TRUE}, only the first attribute is converted into an \code{sf} object. If \code{na.rm} is \code{FALSE}, areas with \code{NA} values are also written out as polygons. Note that the resulting polygons are typically invalid, and use \link[lwgeom]{st_make_valid} to create valid polygons out of them.
 #' @export
 #' @examples
 #' tif = system.file("tif/L7_ETMs.tif", package = "stars")
@@ -87,11 +88,12 @@ st_xy2sfc = function(x, as_points, ..., na.rm = TRUE) {
 #' x
 #' (p = st_as_sf(x)) # removes NA areas
 #' (p = st_as_sf(x[,,,1], merge = TRUE)) # glues polygons together
+#' all(st_is_valid(p)) # not all valid, see details
 #' plot(p, axes = TRUE)
 #' (p = st_as_sf(x, na.rm = FALSE, merge = TRUE)) # includes polygons with NA values
 #' plot(p, axes = TRUE)
 st_as_sf.stars = function(x, ..., as_points = FALSE, merge = FALSE, na.rm = TRUE, 
-		use_integer = is.logical(x[[1]]) || is.integer(x[[1]]), long = FALSE) { 
+		use_integer = is.logical(x[[1]]) || is.integer(x[[1]]), long = FALSE, connect8 = FALSE) { 
 
 	crs = st_crs(x)
 	if (merge && !as_points && has_raster(x) && !any(is.na(get_geotransform(x)))) { # uses GDAL polygonize path:
@@ -101,8 +103,14 @@ st_as_sf.stars = function(x, ..., as_points = FALSE, merge = FALSE, na.rm = TRUE
 				mask
 			} else
 				NULL
-		ret = gdal_polygonize(x, mask, use_integer = use_integer, geotransform = get_geotransform(x),
-				use_contours = FALSE, ...)
+		ret = if (utils::packageVersion("sf") <= "0.7.2") {
+				if (connect8)
+					stop("sf version >= 0.7-3 needed for option connect8")
+				gdal_polygonize(x, mask, use_integer = use_integer, geotransform = get_geotransform(x),
+					use_contours = FALSE, ...)
+			} else
+				gdal_polygonize(x, mask, use_integer = use_integer, geotransform = get_geotransform(x),
+					use_contours = FALSE, connect8 = connect8)
 		# factor levels?
 		if (!is.null(lev <- attr(x[[1]], "levels")))
 			ret[[1]] = structure(ret[[1]], class = "factor", levels = lev)
