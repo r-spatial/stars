c44a96400810cb417fae0d22f36abdec4a076e3c
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-02-11 21:25:17 +0100

    try..

--- tests/stars.R
+++ tests/stars.R
@@ -25,6 +25,7 @@ st_coordinates(x)[1:10,]
 
 # if (version$os == "linux-gnu") { # FIXME: breaks on windows
  nc = paste0("NETCDF:", system.file("nc/tos_O1_2001-2002.nc", package = "stars"), ":tos")
+ print(nc)
  x = read_stars(nc)
  print(x)
  print(st_as_stars(st_bbox(x)))
