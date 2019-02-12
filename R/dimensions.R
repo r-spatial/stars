ba811a3a4864cf6a92e06ea2df89ec8c19a06420
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-02-12 10:30:25 +0100

    debug appveyor...

--- R/dimensions.R
+++ R/dimensions.R
@@ -279,12 +279,11 @@ get_val = function(pattern, meta) {
 
 parse_netcdf_meta = function(pr, name) {
 	meta = pr$meta
-	print(paste0("name:", name))
-	#name = if (!grepl(":", name, fixed = TRUE) || grepl("\\1", name, fixed = TRUE)) # name is not the variable, but the file name; FIXME: how to make it the variable?
-	name = if (!grepl(":", name, fixed = TRUE) || grepl("\\1", name, fixed = TRUE)) # name is not the variable, but the file name; FIXME: how to make it the variable?
+	spl = strsplit(name, ":")[[1]] # "C:\..." results in length 2:
+	name = if (length(spl) < 3) # name is not the variable, but the file name; FIXME: how to make it the variable?
 			"zzzzz40163a99980" # bogus string, to avoid match
 		else
-			tail(strsplit(name, ":")[[1]], 1)
+			tail(spl, 1) # last one contains 
 	# unit:
 	pr$units = get_val(paste0(name, "#units"), meta)
 	# extra dims: NETCDF_DIM_EXTRA={time,zlev}
