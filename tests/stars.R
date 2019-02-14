dd308cbd861bb520f07d7d20cf6a8d8e9c8f1929
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-02-14 21:23:30 +0100

    smooth out some CI errors

--- tests/stars.R
+++ tests/stars.R
@@ -10,6 +10,15 @@ plot(x, join_zlim = FALSE)
 x %>% st_set_dimensions(names = c('a', 'b', 'c'))
 st_get_dimension_values(x, 3)
 
+(x1 = st_set_dimensions(x, "band", values = c(1,2,3,4,5,7), names = "band_number", point = TRUE))
+rbind(c(0.45,0.515), c(0.525,0.605), c(0.63,0.69), c(0.775,0.90), c(1.55,1.75), c(2.08,2.35)) %>%
+	units::set_units(um) -> bw # units::set_units(µm) -> bw
+# set bandwidth midpoint:
+(x2 = st_set_dimensions(x, "band", values = 0.5 * (bw[,1]+bw[,2]), 
+   names = "bandwidth_midpoint", point = TRUE))
+# set bandwidth intervals:
+(x3 = st_set_dimensions(x, "band", values = make_intervals(bw), names = "bandwidth"))
+
 x + x
 x * x
 x[,,,1:3]
