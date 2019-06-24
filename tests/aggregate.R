24614cc61381b0da15da14398a6259bc632c11d9
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-06-24 09:33:07 +0200

    tidy test

--- tests/aggregate.R
+++ tests/aggregate.R
@@ -3,7 +3,7 @@ library(stars)
 library(spacetime)
 data(air) # this loads several datasets in .GlobalEnv
 dim(air)
-d = st_dimensions(station = st_as_sfc(stations), time = dates)
+d = st_dimensions(station = st_set_crs(st_as_sfc(stations), 4326), time = dates)
 
 blocks = st_make_grid(st_as_sfc("POLYGON ((5.871619 47.26986, 15.03811 47.26986, 15.03811 55.05653, 5.871619 55.05653, 5.871619 47.26986))", crs = 4326),
 	n = c(3,3))
