ffc5fc49b87f3db78dcf40a609c28bbf9e5f7316
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-08-17 09:42:41 +0200

    use on.exit(), set names, add example; #210

--- R/mosaic.R
+++ R/mosaic.R
@@ -8,6 +8,11 @@
 #' 
 #' uses \link[sf]{gdal_utils} to internally call \code{buildvrt}; no executables external to R are called.
 #' @export
+#' @examples
+#' x = read_stars(system.file("tif/L7_ETMs.tif", package = "stars"))
+#' x1 = x[,100:200,100:200,]
+#' x2 = x[,150:300,150:300,]
+#' plot(st_mosaic(x1, x2))
 st_mosaic = function(..., options = c("-vrtnodata", "-9999")) {
 	lst_write = function(obj) {
 		fname = tempfile(fileext = ".tif")
@@ -16,8 +21,7 @@ st_mosaic = function(..., options = c("-vrtnodata", "-9999")) {
 	}
 	src = sapply(list(...), lst_write)
 	dst = tempfile(fileext = ".tif")
+	on.exit(unlink(c(dst, src)))
 	sf::gdal_utils("buildvrt", src, dst, options)
-	ret = read_stars(dst)
-	unlink(c(dst, src))
-	ret
+	setNames(read_stars(dst), names(list(...)[[1]])[1])
 }
