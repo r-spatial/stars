f0c163ad89d058a748f5dca91de666164506665e
David Blodgett <dblodgett@usgs.gov>
2019-08-28 12:21:55 -0500

    add st_as_stars method for ncdfgeom

--- R/ncdf.R
+++ R/ncdf.R
@@ -661,4 +661,45 @@ make_cal_time2 <- function(dimension, time_name, time_unit = NULL, cal = NULL) {
     
 }
 
+#' @param sf_geometry sf data.frame with geometry and attributes to be added to stars object. 
+#' Must have same number of rows as timeseries instances.
+#' @details For the \code{ncdfgeom} method: objects are point-timeseries with optional line or polygon geometry for each timeseries specified with the \code{sf_geometry} parameter. See \pkg{ncdfgeom} for more about this NetCDF-based format for geometry and timeseries.
+#' @name st_as_stars
+#' @export
+#' 
+st_as_stars.ncdfgeom <- function(.x, ..., sf_geometry = NA) {
+  
+  crs <- sf::st_crs(4326)$proj4string
+  ts_points <- data.frame(X = .x$lons, Y = .x$lats, Z = .x$alts)
+  ts_points <- sf::st_as_sf(ts_points, coords = c("X", "Y", "Z"), crs = crs)
+  
+  data <- .x$data_frames[[1]]
+  
+  gdim <- create_dimension(from = 1, to = length(.x$lats), 
+                           refsys = crs, point = TRUE, 
+                           values = ts_points$geometry)
+  tdim <- create_dimension(from = 1, to = length(.x$time), 
+                           refsys = "POSIXct", point = FALSE, 
+                           values = as.POSIXct(.x$time))
+  dim <- list(time = tdim, points = gdim)
+  
+  if("sf" %in% class(sf_geometry)) {
+    if(length(gdim$values) != length(st_geometry(sf_geometry))) 
+      stop("geometry must be same length as instance dimension of timeseries")
+    
+    is_point <- any(grepl("point", class(st_geometry(sf_geometry)), ignore.case = TRUE))
+    
+    sf_dim <- create_dimension(from = 1, to = length(gdim$values),
+                               refsys = st_crs(sf_geometry)$proj4string, 
+                               point = is_point, is_raster = FALSE,
+                               values = st_geometry(sf_geometry))
+    
+    dim <- c(dim, list(geometry = sf_dim))
+  }
+  
+  st_stars(x = setNames(list(as.matrix(.x$data_frames[[1]])), 
+                        .x$varmeta[[1]]$name), 
+           dimensions =  create_dimensions(dim))
+}
+
 
