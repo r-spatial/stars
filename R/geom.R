572ffb8f5af7c1aa833a5e2d184a4bf3d59e8ff9
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-08-30 16:51:52 +0200

    update

--- R/geom.R
+++ R/geom.R
@@ -34,7 +34,7 @@ st_intersects.stars = function(x, y, sparse = TRUE, ..., as_points = NA, transpo
 		else
 			ret
 		
-	} else { # rectilinear, curvilinear, or polygons:
+	} else { # curvilinear:
 		ret = st_intersects(st_as_sf(x, as_points = as_points), y, sparse = sparse, ...)
 		if (! sparse)
 			ret = as.matrix(ret)
@@ -48,21 +48,35 @@ st_intersects.stars = function(x, y, sparse = TRUE, ..., as_points = NA, transpo
 
 # FIXME: of_y should be left/right/full?
 
+#' Spatially join a stars and an `sf` object
+#'
+#' Spatially join a stars and an `sf` object
+#' @param x object of class stars
+#' @param y object of class sf, or one that can be coerced into that by \link{st_as_sf}
+#' @param join join function, which should return an sgbp object
+#' @param ... arguments that will be passed on to the join function
+#' @param as_points logical; controls whether grid cells in \code{x} will be treated as points, or as cell areas; \link{st_intersects.stars} by default will derive this from \code{x}'s metadata, or else assume areas.
+#' @param what either "left1" or "right"
+#' @param warn logical; if TRUE, warn on 1-to-many matches when \code{what} is \code{"left1"}
+#' @returns If what is "left1", an object of class stars with the (first) value of y at spatial instances of x: when there is more than one match to a single x value, the first matching record from y is taken (and if \code{warn} is TRUE a warning is raised). If what is "inner", an object of class \code{sf} with all matching records of x and y.
 #' @export
-st_join.stars = function(x, y, ..., what = "left") {
+st_join.stars = function(x, y, ..., join = st_intersects, what = "left", as_points = NA, warn = TRUE) {
 	if (!requireNamespace("dplyr", quietly = TRUE)) 
 		stop("dplyr needed: install first?")
-	if (inherits(y, "sfc"))
-		y = st_sf(y)
-	if (!inherits(y, "sf"))
-		stop("argument y should be of class sf, or sfc")
+
+	if (!inherits(y, "sf")) {
+		try(y <- st_as_sf(y))
+		if (inherits(y, "try-error"))
+			stop("argument y should be of class sf, or sfc")
+	}
+
 	x = st_upfront(x)
 
-	if (what == "left") {
-		ix = st_intersects(x, y, sparse = TRUE, ..., transpose = FALSE)
+	if (what == "left1") {
+		ix = join(x, y, ..., as_points = as_points)
 		li = lengths(ix)
-		if (any(li > 1)) {
-			warning("st_join has n-to-m matches, taking first match for each n")
+		if (warn && any(li > 1)) {
+			warning(paste("st_join found", sum(li > 1),"1-to-n matches, taking the first match for each of those"))
 			ix[li > 1] = lapply(ix[li > 1], head, 1)
 		}
 		sel = li > 0
@@ -77,8 +91,11 @@ st_join.stars = function(x, y, ..., what = "left") {
 			x[[ i ]] = a
 		}
 		x
-	} else if (what == "right") {
-		ix = st_intersects(x, y, sparse = TRUE, ..., transpose = TRUE)
+	} else if (what == "inner") {
+		ix = if (missing(join))
+				st_intersects(x, y, sparse = TRUE, ..., transpose = TRUE, as_points = as_points)
+			else
+				join(y, st_as_sf(x, as_points = as_points), ...)
 		li = lengths(ix)
 		i = unlist(ix) # sequence of x indexes
 		j = rep(seq_along(ix), li) # possibly repeated y indexes
