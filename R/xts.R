0a61449087c802337d40fb91af9f4ed6602f4f29
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-11-23 16:09:19 +0100

    robustify as.xts.stars

--- R/xts.R
+++ R/xts.R
@@ -1,6 +1,5 @@
 as.xts.stars <- function(x,...) { 
 
-	stopifnot(length(dim(x)) == 2)
 	if (!requireNamespace("xts", quietly = TRUE))
 		stop("xts required: install that first") # nocov
 	if (length(x) > 1)
@@ -8,5 +7,38 @@ as.xts.stars <- function(x,...) {
 
 	ed = expand_dimensions.stars(x)
 	time = which(sapply(ed, inherits, c("Date", "POSIXt")))
-	xts::xts(st_upfront(x[1], time)[[1]], ed[[time]])
+	if (length(time) < 1)
+		stop("no time dimension present in object")
+	if (length(time) > 1) {
+		message("more than one time dimension present in object; taking the first")
+		time = time[1]
+	}
+	x = st_upfront(x[1], time)
+	if (length(dim(x)) > 2)
+		x = st_redimension(x, c(dim(x)[1], prod(dim(x)[-1])))
+	
+	xts::xts(x[[1]], ed[[time]])
+}
+
+#' @name st_as_stars
+#' @details for the \code{xts} methods, if \code{dimensions} are provided, time has to be the first dimension.
+#' @export
+st_as_stars.xts = function(.x, ..., dimensions) {
+	if (!requireNamespace("xts", quietly = TRUE))
+		stop("xts required: install that first") # nocov
+	if (!requireNamespace("zoo", quietly = TRUE))
+		stop("zoo required: install that first") # nocov
+	time = zoo::index(x)
+	cn = colnames(x)
+	if (!missing(dimensions)) {
+		x = as.matrix(x)
+		dim(x) = dim(dimensions)
+		st_stars(list(x), dimensions)
+	} else {
+		x = st_as_stars(list(as.matrix(x)))
+		x = st_set_dimensions(x, 1, time)
+		if (!is.null(cn))
+			x = st_set_dimensions(x, 2, cn)
+		st_set_dimensions(x, names = c("time", "others"))
+	}
 }
