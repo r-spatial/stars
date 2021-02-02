cbd65784f1626d0e90c438e00b55844b876eb3d7
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2021-02-02 10:33:35 +0100

    collapse multi-file proxy; fixes #385

--- R/warp.R
+++ R/warp.R
@@ -168,6 +168,12 @@ st_warp = function(src, dest, ..., crs = NA_crs_, cellsize = NA_real_, segments 
 			else
 				on.exit(unlink(src[[1]])) # temp file
 		}
+		# collapse a multi-file proxy objects into single-file/multi-band:
+		if (length(src[[1]]) > 1) {
+			tmp = tempfile()
+			sf::gdal_utils("buildvrt", src[[1]], tmp, options = "-separate")
+			src[[1]] = tmp
+		}
 		if (missing(dest) && is.na(crs)) {
 			delete = TRUE
 			dest = tempfile(fileext = ".tif")
