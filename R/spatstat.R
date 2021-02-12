2d33f5b0876852067adbc48d64976ba8a7faed81
rubak <rubak@math.aau.dk>
2021-02-13 00:37:45 +0100

    Update to spatstat 2.0-0 (and spatstat.geom).

--- R/spatstat.R
+++ R/spatstat.R
@@ -1,11 +1,22 @@
+check_spatstat <- function(pkg, X = NULL){
+  if(!requireNamespace(pkg, quietly = TRUE)){
+    stop("package ", pkg, " required, please install it (or the full spatstat package) first")
+  } else{
+    spst_ver <- try(packageVersion("spatstat"), silent = TRUE)
+    if(!inherits(spst_ver, "try-error") && spst_ver < 2.0-0){
+      stop("You have an old version of spatstat installed which is incompatible with ", pkg, 
+           ". Please update spatstat (or uninstall it).")
+    }
+  }
+  if (!is.null(X) && isTRUE(st_is_longlat(X)))
+    stop("Only projected coordinates may be converted to spatstat class objects")
+}
+
 as.owin.stars = function(W, ..., fatal) {
-	if (isTRUE(st_is_longlat(W)))
-		stop("Only projected coordinates may be converted to spatstat class objects")
-	if (!requireNamespace("spatstat", quietly = TRUE))
-		stop("package spatstat required: install first?")
+  check_spatstat("spatstat.geom", W)
 	if (length(dim(W)) != 2)
 		stop("as.owin.stars requires a 2-dimensional object (single raster layer)")
 	m = t(W[[1]])
 	bb = st_bbox(W)
-	spatstat::owin(bb[c("xmin", "xmax")], bb[c("ymin", "ymax")], mask = m[nrow(m):1,])
+	spatstat.geom::owin(bb[c("xmin", "xmax")], bb[c("ymin", "ymax")], mask = m[nrow(m):1,])
 }
