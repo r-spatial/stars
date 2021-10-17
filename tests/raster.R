6529f4940a0e7899cdc2f78c74d8453ee1b882b7
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-10-17 11:58:22 +0200

    adds terra cests

--- tests/raster.R
+++ tests/raster.R
@@ -7,11 +7,26 @@ tif = system.file("tif/L7_ETMs.tif", package = "stars")
 (y = st_as_stars(r))
 
 # single band:
-x = adrop(x[,,,1]) 
+x = adrop(x[,,,1])
 r = as(x, "Raster")
 (y = st_as_stars(r))
 
 # proxy:
 (x = read_stars(tif, proxy = TRUE))
 (r = as(x, "Raster"))
+(y = st_as_stars(r))
+
+## terra -------------
+(x = read_stars(tif))
+(r = as(x, "SpatRaster"))
+(y = st_as_stars(r))
+
+# single band:
+x = adrop(x[,,,1])
+r = as(x, "SpatRaster")
+(y = st_as_stars(r))
+
+# proxy:
+(x = read_stars(tif, proxy = TRUE))
+(r = as(x, "SpatRaster"))
 (y = st_as_stars(r))
