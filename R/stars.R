a56148e91a5deef057bc865ca640a9ab7aa871c3
David Blodgett <dblodgett@usgs.gov>
2021-12-22 14:53:39 -0600

    move add_units into package environment and call from print.nc_proxy. Start playing with vignette.

--- R/stars.R
+++ R/stars.R
@@ -9,7 +9,9 @@ split_strings = function(md, split = "=") {
 #' convert objects into a stars object
 #' @export
 #' @param .x object to convert
-#' @param ... in case \code{.x} is of class \code{bbox}, arguments passed on to \link{pretty}
+#' @param ... in case \code{.x} is of class \code{bbox}, arguments passed on to 
+#' \link{pretty}. In case \code{.x} is of class \code{nc_proxy}, arguments
+#' passed on to \code{\link{read_ncdf}}.
 st_as_stars = function(.x, ...) UseMethod("st_as_stars")
 
 #' @name st_as_stars
@@ -371,12 +373,13 @@ as.data.frame.stars = function(x, ..., add_max = FALSE, center = NA) {
 		lapply(x, function(y) structure(y, dim = NULL)))
 }
 
+add_units = function(x) {
+	f = function(obj) if (inherits(obj, "units")) paste0("[", enc2utf8(as.character(units(obj))), "]") else ""
+	paste(names(x), sapply(x, f))
+}
+
 #' @export
 print.stars = function(x, ..., n = 1e5, abbrev = 30) {
-	add_units = function(x) {
-		f = function(obj) if (inherits(obj, "units")) paste0("[", enc2utf8(as.character(units(obj))), "]") else ""
-		paste(names(x), sapply(x, f))
-	}
 	shorten = function(s) {
 		if (nchar(s) > abbrev)
 			paste0(substr(s, 1, abbrev), "...")
