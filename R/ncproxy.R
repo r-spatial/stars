bb10d332bf507e1d44ad6f0a4c4c1115e41e6cbf
David Blodgett <dblodgett@usgs.gov>
2021-12-27 08:20:33 -0600

    rudimentary plot method for nc_proxy

--- R/ncproxy.R
+++ R/ncproxy.R
@@ -1,5 +1,9 @@
 # nc_proxy is a subclass of stars_proxy.
 
+get_nc_request <- function(x) {
+	attr(x, "nc_request")
+}
+
 # nc_request is a class for "nc_request" attributes of an nc_proxy object.
 #' @export
 print.nc_request <- function(x) {
@@ -33,7 +37,7 @@ print.nc_proxy = function(x, ..., n = 1e5, nfiles = 10, simplify = TRUE) {
 	cat("netcdf source stars proxy object from:\n")
 
 	if(simplify) print(shorten_names(x[[1]], nfiles))
-	nc_request <- attr(nc, "nc_request")
+	nc_request <- get_nc_request(nc)
 
 	cat("\nAvailable nc variables:\n")
 	cat(paste(names(nc_request),
@@ -47,7 +51,26 @@ print.nc_proxy = function(x, ..., n = 1e5, nfiles = 10, simplify = TRUE) {
 	print(nc_request)
 }
 
+# as.data.frame.stars_proxy works for nc_proxy.
+
 #' @export
-st_as_stars.nc_proxy <- function(x, ...) {
-	read_ncdf(x, ...)
+st_as_stars.nc_proxy <- function(x, downsample = 0, ...) {
+	read_ncdf(x, downsample = 0, ...)
+}
+
+#' @name plot
+#' @param max_times integer; maximum number of time steps to attempt to plot. 
+#' @export
+#' @details when plotting a subsetted \code{stars_proxy} object, the default value for argument \code{downsample} will not be computed correctly, and has to be set manually.
+plot.nc_proxy = function(x, y, ..., downsample = get_downsample(dim(x)), max_times = 10) {
+	
+	nc_request <- get_nc_request(x)
+	
+	nc_request <- nc_request[1]
+			
+	nc_request[[1]]$count[nc_request[[1]]$axis == "T"] <- max_times
+	
+	attr(x, "nc_request") <- nc_request
+	
+	plot(st_as_stars(x, downsample = downsample, ...), ..., downsample = 0)
 }
