096ccf4c626bce43a9b30525fbb8c18fecd87d9d
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2021-12-27 17:31:18 +0100

    add `overlap` arg

--- tests/testthat/test_tile.R
+++ tests/testthat/test_tile.R
@@ -26,3 +26,10 @@ test_that("many small blocks ", {
 	expect_setequal(test4[, "nXSize"], rep(1, times = 100))
 	expect_setequal(test4[, "nYSize"], rep(1, times = 100))
 })
+
+test5 = st_tile(nrow(mat), ncol(mat), 5, 5, overlap = 5)
+test_that("blocks with overlap", {
+	expect_equal(nrow(test5), 4)
+	expect_setequal(test5[, "nXSize"], rep(c(10, 5), each = 2))
+	expect_setequal(test5[, "nYSize"], rep(c(10, 5), times = 2))
+})
