661138bedb0f67305c6af481bb6b625c5afb417c
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2022-08-12 22:10:52 +0200

    fixes

--- R/mdim.R
+++ R/mdim.R
@@ -97,12 +97,34 @@ add_units_attr = function(l) {
 		lapply(l, f)
 }
 
+cdl_add_polygons = function(e, i, sfc) {
+	stopifnot(inherits(sfc, "sfc_MULTIPOLYGON"))
+	cc = st_coordinates(sfc)
+	e$node = structure(seq_len(nrow(cc)), dim = c(node = nrow(cc)))
+	e$x = structure(cc[, 1], dim = c(node = nrow(cc)))
+	e$y = structure(cc[, 2], dim = c(node = nrow(cc)))
+ # int node_count(instance) ;
+ # int part_node_count(part) ;
+ # int interior_ring(part) ;
+	e$node_count = structure(rle(cc[,"L3"])$lengths, dim = setNames(length(sfc), names(e)[i]))
+	part = rle(cc[,"L3"] * 2 * max(cc[,"L2"]) + 2 * (cc[,"L2"] - 1) + cc[,"L1"])$lengths
+	e$part_node_count = structure(part, dim = c(part = length(part)))
+	e$interior_ring = structure(as.numeric(cc[cumsum(part), "L1"] > 1), dim = c(part = length(part)))
+	attr(e, "dims") = c(node = nrow(cc), part = length(part))
+	e$geometry = add_attr(structure(numeric(0), dim = c("somethingNonEx%isting" = 0)),
+		c(geometry_type = "polygon", node_count = "node_count", node_coordinates = "x y",
+		  part_node_count = "part_node_count", interior_ring = "interior_ring",
+		  grid_mapping = if (is.na(st_crs(sfc))) "crs" else NULL))
+	e
+}
+
 # convert stars object into a list of CDL-like variables with named dimensions
 st_as_cdl = function(x) {
 	e = add_units_attr(expand_dimensions(x)) # TODO: handle sfc dimensions
 	d = st_dimensions(x)
 	dimx = dim(x)
 	xy = attr(st_dimensions(x), "raster")$dimensions
+	co = NULL
 	for (i in seq_along(e)) {
 		if (is.null(dim(e[[i]])) && !is.list(e[[i]]))
 			e[[i]] = structure(e[[i]], dim = setNames(length(e[[i]]), names(e)[i]))
@@ -112,26 +134,43 @@ st_as_cdl = function(x) {
 		} else if (inherits(e[[i]], "sfc")) { # vector data cube:
 			sfc = e[[i]]
 			e[[i]] = structure(seq_along(sfc), dim = setNames(length(sfc), names(e)[i]))
-			if (!inherits(sfc, "sfc_POINT"))
+			if (inherits(sfc, "sfc_POINT")) {
+				cc = st_coordinates(sfc)
+				if (isTRUE(st_is_longlat(x))) {
+					e$lon = add_attr(structure(cc[, 1], dim = setNames(nrow(cc), names(e)[i])), 
+								 	c(units = "degrees_north", standard_name = "longitude"))
+					e$lat = add_attr(structure(cc[, 2], dim = setNames(nrow(cc), names(e)[i])), 
+								 	c(units = "degrees_east", standard_name = "latitude"))
+					co = c(coordinates = "lat lon")
+					xy = c("lon", "lat") # hack
+				} else {
+					e$x = structure(cc[, 1], dim = setNames(nrow(cc), names(e)[i]))
+					e$y = structure(cc[, 2], dim = setNames(nrow(cc), names(e)[i]))
+					co = c(coordinates = "x y")
+					xy = c("x", "y") # hack
+				}
+			} else if (inherits(sfc, c("sfc_MULTIPOLYGON", "sfc_POLYGON"))) {
+				e = cdl_add_polygons(e, i, st_cast(sfc, "MULTIPOLYGON"))
+				dimx = c(dimx, attr(e, "dims"))
+				co = c(coordinates = "x y")
+				xy = c("x", "y") # hack
+			} else
 				stop("only support for sfc_POINT so far")
-			cc = st_coordinates(sfc)
-			e$x = structure(cc[, 1], dim = setNames(nrow(cc), names(e)[i]))
-			e$y = structure(cc[, 2], dim = setNames(nrow(cc), names(e)[i]))
 		}
 	}
 
 	if (is_curvilinear(x)) {
 		e[["lat"]] = add_attr(e[["lat"]], c(units = "degrees_north", "_CoordinateAxisType" = "Lat", axis = "Y"))
 		e[["lon"]] = add_attr(e[["lon"]], c(units = "degrees_east",  "_CoordinateAxisType" = "Lon", axis = "X"))
-		cc = paste(rev(setdiff(names(d), c("x", "y", "lat", "lon"))), "lat lon")
-		for (i in seq_along(x))
-			x[[i]] = add_attr(x[[i]], c(coordinates = cc))
+		co = paste(rev(setdiff(names(d), c("x", "y", "lat", "lon"))), "lat lon")
 	} else {
 		if (!any(is.na(xy))) {
 			e[[ xy[1] ]] = add_attr(e[[ xy[1] ]], c(axis = "X"))
 			e[[ xy[2] ]] = add_attr(e[[ xy[2] ]], c(axis = "Y"))
 		}
 	}
+	for (i in seq_along(x))
+		x[[i]] = add_attr(x[[i]], co)
 
 	x = add_units_attr(x) # unclasses
 	for (i in seq_along(x))
@@ -139,8 +178,15 @@ st_as_cdl = function(x) {
 			names(dim(x[[i]])) = names(d)
 	for (i in names(e))
 		x[[i]] = e[[i]]
-	ret = lapply(x, function(a) structure(a, which_dims = match(names(dim(a)), names(dimx)) - 1))
-	structure(ret, which_crs = !(names(ret) %in% names(x)))
+	which_dims = function(a, dimx) {
+		m = match(names(dim(a)), names(dimx), nomatch = numeric(0)) - 1
+		if (all(is.na(m)))
+			integer(0)
+		else
+			m
+	}
+	ret = lapply(x, function(a) structure(a, which_dims = which_dims(a, dimx)))
+	structure(ret, which_crs = !(names(ret) %in% names(e)), dims = attr(e, "dims"))
 }
 
 
@@ -175,7 +221,7 @@ write_mdim = function(x, filename, driver = detect.driver(filename), ...,
 		else
 			st_crs(x)$wkt
 	xy = attr(st_dimensions(x), "raster")$dimensions
-	gdal_write_mdim(filename, driver, dim(x), cdl, wkt, xy, 
+	gdal_write_mdim(filename, driver, c(dim(x), attr(cdl, "dims")), cdl, wkt, xy, 
 					root_group_options = root_group_options, options = options)
 	invisible(x)
 }
