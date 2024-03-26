08bd74a555fecb689d50d45fd829a08363ef75ff
loreabad6 <lore.abad6@gmail.com>
2024-03-26 14:18:35 +0100

    Experiments

--- R/extract.R
+++ R/extract.R
@@ -38,7 +38,7 @@ st_extract.stars = function(x, at, ..., bilinear = FALSE, time_column =
 		pts = at
 	else {
 		stopifnot(st_crs(at) == st_crs(x))
-		if (! all(st_dimension(at) == 0)) { # should check & branch here in case of MULTIPOINT?
+		if (! all(st_dimension(at) >= 0)) { # should check & branch here in case of MULTIPOINT?
 			stopifnot(is.null(time_column), !bilinear, !interpolate_time)
 			# from aggregate.stars_proxy:
 			by = st_geometry(at)
